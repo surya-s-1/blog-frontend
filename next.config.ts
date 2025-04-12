38acd3e75920086810c5e9e60d2e9fe0a22180e1
Surya S <suryah2806@gmail.com>
2025-04-12 13:06:47 +0530

    added custom svg and removed default content

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/blog',
+  // assetPrefix: '/todo',
+  // trailingSlash: true,
+  // skipTrailingSlashRedirect: true,
+  // output: 'export',
   reactStrictMode: true,
+  devIndicators: false
 };
 
 export default nextConfig;
