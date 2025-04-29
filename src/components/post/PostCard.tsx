db71cd94af2c002ebc8df22121ae81be2fe5f1e6
Surya S <suryah2806@gmail.com>
2025-04-29 14:50:30 +0530

    add comment and share buttons to PostCard component; update dull button styles

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 
 import Metadata from '@/components/post/Metadata'
 import { PostCardInterface, TagsInterface } from '@/components/interfaces/Post'
+import { FaRegComment, FaShare } from 'react-icons/fa'
 
 function Tags({ tags, display }: TagsInterface) {
     if (tags.length === 0) return null
@@ -52,20 +53,27 @@ export default function PostCard({ post, display }: PostCardInterface) {
                 />
                 <p className='p-2'>{post.content.slice(0, 300).trim().concat(concatRequired ? '...' : '')}</p>
                 <Tags tags={post.tags} display={display} />
+                <div className='w-full flex flex-row items-center justify-evenly'>
+                    <button className='button-dull'><FaRegComment size={16} /> Comment</button>
+                    <button className='button-dull'><FaShare size={16} /> Share</button>
+                </div>
             </div>
         )
     } else {
         return (
             <div className='w-full h-fit bg-default-bg rounded-lg p-4 pb-6 m-2 flex flex-col gap-1'>
-                <Metadata
-                    firstName={post.firstName}
-                    middleName={post.middleName}
-                    lastName={post.lastName}
-                    dp={post.dp}
-                    username={post.username}
-                    display={display}
-                    timestamp={post.createdAt}
-                />
+                <div className='w-full flex flex-row items-center justify-between'>
+                    <Metadata
+                        firstName={post.firstName}
+                        middleName={post.middleName}
+                        lastName={post.lastName}
+                        dp={post.dp}
+                        username={post.username}
+                        display={display}
+                        timestamp={post.createdAt}
+                    />
+                    <button className='button-dull text-lg'><FaShare size={20} /> Share</button>
+                </div>
                 <div className='p-2'>{post.content}</div>
                 <Tags tags={post.tags} display={display} />
             </div>
